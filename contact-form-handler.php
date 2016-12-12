0363a2806f2d2bdc66a628cdd5838e5feb686c02
Beumsk <beumieremy@gmail.com>
2016-12-12 16:05:03 +0100

    the form is now working with link to my email (thx php) ! & Images size changed to fit easier in carousel

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -24,13 +24,37 @@ if( empty($errors))
 	$to = $myemail; 
 	$email_subject = "Contact form submission: $name";
 	$email_body = "You have received a new message. ".
-	" Here are the details:\n Name: $name \n Email: $email_address \n Message \n $message"; 
+	" Here are the details:\nName: $name \nEmail: $email_address \nMessage: \n\n$message"; 
 	
 	$headers = "From: $myemail\n"; 
 	$headers .= "Reply-To: $email_address";
 	
 	mail($to,$email_subject,$email_body,$headers);
 	//redirect to the 'thank you' page
-	header('Location: index.html#contact');
+	header('Location: http://www.remybeumier.esy.es/#contact');
 } 
 ?>
+<!DOCTYPE html> 
+<html>
+<head>
+	<title>Contact form handler</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<div class="well text-center">
+	<?php
+	echo nl2br($errors);
+	?>
+
+	<br>
+	<p>Wait 2 seconds or be fast enough to click on the link ! --> <a href="http://www.remybeumier.esy.es/#contact">Send me back !</a></p>
+</div>
+
+<?php
+header('refresh:2; url=http://www.remybeumier.esy.es/#contact'); // go back to contact after 2sec
+?>
+
+</body>
+</html>
